d3239bc3a8976fc23389b3befe0b6199c97f5e7d
Michael Dales <mwd24@cam.ac.uk>
2023-07-11 13:04:45 +0100

    Try using queues to stream data

--- methods/matching/find_potential_matches.py
+++ methods/matching/find_potential_matches.py
@@ -1,28 +1,87 @@
 import glob
 import os
 import sys
-from functools import partial
-from multiprocessing import Pool
-from typing import List, Tuple
+from dataclasses import dataclass
+from multiprocessing import Manager, Process, Queue
+from typing import Tuple
 
 import numpy as np
 import pandas as pd
 from yirgacheffe.layers import RasterLayer  # type: ignore
+import yirgacheffe.operators
 
 from methods.matching.calculate_k import build_layer_collection
 
-def find_potential_matches_per_k_item(
+yirgacheffe.operators.YSTEP = 1024 * 12
+
+@dataclass
+class MatchedPixel:
+    k_lat: float
+    k_lng: float
+    match_lat: float
+    match_lng: float
+    xoffset: int
+    yoffset: int
+    ecoregion: int
+    elevation: float
+    slope: float
+    access: float
+    luc0: float
+
+    def __eq__(self, val):
+        return (self.xoffset, self.yoffset) == val
+
+    def __ne__(self, val):
+        return (self.xoffset, self.yoffset) != val
+
+    def __hash__(self):
+        return (self.xoffset, self.yoffset).__hash__()
+
+
+def threads() -> int:
+    return 100
+
+
+def load_k(k_filename: str, queue: Queue) -> None:
+    # put the source pixels into the queue
+    source_pixels = pd.read_parquet(k_filename)
+    for _, row in source_pixels.iterrows():
+        queue.put(row)
+    # To close the pipe put in one sentinel value per worker
+    for _ in range(threads()):
+        queue.put(None)
+
+def reduce_results(
+    result_dataframe_filename: str,
+    queue: Queue
+) -> None:
+    sentinal_count = threads()
+    results = set()
+    while True:
+        vals = queue.get()
+        if vals is None:
+            sentinal_count -= 1
+            if sentinal_count == 0:
+                break
+        else:
+            results.update(vals)
+
+    output = pd.DataFrame(results)
+    output.to_parquet(result_dataframe_filename)
+
+def worker(
+    worker_index: int,
     matching_zone_filename: str,
     jrc_data_folder: str,
     ecoregions_folder_filename: str,
     elevation_folder_filename: str,
     slope_folder_filename: str,
     access_folder_filename: str,
-    year: int,
-    matching_row: Tuple
-) -> Tuple[int,int,List]:
-    index, matching = matching_row
-
+    start_year: int,
+    _evaluation_year: int,
+    input_queue: Queue,
+    output_queue: Queue,
+) -> None:
     # everything is done at JRC resolution, so load a sample file from there first to get the ideal pixel scale
     example_jrc_filename = glob.glob("*.tif", root_dir=jrc_data_folder)[0]
     example_jrc_layer = RasterLayer.layer_from_file(os.path.join(jrc_data_folder, example_jrc_filename))
@@ -30,7 +89,7 @@ def find_potential_matches_per_k_item(
     matching_collection = build_layer_collection(
         example_jrc_layer.pixel_scale,
         example_jrc_layer.projection,
-        year,
+        start_year,
         matching_zone_filename,
         jrc_data_folder,
         ecoregions_folder_filename,
@@ -39,60 +98,67 @@ def find_potential_matches_per_k_item(
         access_folder_filename,
     )
 
-    matching_pixels = RasterLayer.empty_raster_layer_like(matching_collection.boundary)#, filename=temp_output)
+    while True:
+        matching = input_queue.get()
+        if matching is None:
+            break
 
-    filtered_ecoregions = matching_collection.ecoregions.numpy_apply(lambda chunk: chunk == matching.ecoregion)
-    filtered_elevation = matching_collection.elevation.numpy_apply(
-        lambda chunk: np.logical_and(chunk >= (matching.elevation - 200), chunk <= (matching.elevation + 200))
-    )
-    filtered_slopes = matching_collection.slope.numpy_apply(
-        lambda chunk: np.logical_and(chunk >= (matching.slope - 2.5), chunk <= (matching.slope + 2.5))
-    )
-    filtered_access = matching_collection.access.numpy_apply(
-        lambda chunk: np.logical_and(chunk >= (matching.access - 10), chunk <= (matching.access + 10))
-    )
-    filtered_luc0 = matching_collection.lucs[0].numpy_apply(lambda chunk: chunk == matching.luc0)
-    filtered_luc5 = matching_collection.lucs[1].numpy_apply(lambda chunk: chunk == matching.luc5)
-    filtered_luc10 = matching_collection.lucs[2].numpy_apply(lambda chunk: chunk == matching.luc10)
+        matching_pixels = RasterLayer.empty_raster_layer_like(matching_collection.boundary)
 
-    calc = matching_collection.boundary * filtered_ecoregions * filtered_elevation * \
-        filtered_luc0 * filtered_luc5 * filtered_luc10 * filtered_slopes * filtered_access
-    count = calc.save(matching_pixels, and_sum=True)
-    results = []
-    if count == 0:
-        print(f"No matching pixels found for index {index}")
-    else:
-        # now we we need to scan for matched pixels and store the data about them
+        filtered_ecoregions = matching_collection.ecoregions.numpy_apply(lambda chunk: chunk == matching.ecoregion)
+        filtered_elevation = matching_collection.elevation.numpy_apply(
+            lambda chunk: np.logical_and(chunk >= (matching.elevation - 200), chunk <= (matching.elevation + 200))
+        )
+        filtered_slopes = matching_collection.slope.numpy_apply(
+            lambda chunk: np.logical_and(chunk >= (matching.slope - 2.5), chunk <= (matching.slope + 2.5))
+        )
+        filtered_access = matching_collection.access.numpy_apply(
+            lambda chunk: np.logical_and(chunk >= (matching.access - 10), chunk <= (matching.access + 10))
+        )
+        filtered_luc0 = matching_collection.lucs[0].numpy_apply(lambda chunk: chunk == matching.luc0)
+        filtered_luc5 = matching_collection.lucs[1].numpy_apply(lambda chunk: chunk == matching.luc5)
+        filtered_luc10 = matching_collection.lucs[2].numpy_apply(lambda chunk: chunk == matching.luc10)
 
-        width = matching_collection.boundary.window.xsize
-        for yoffset in range(matching_collection.boundary.window.ysize):
-            row_matches = matching_pixels.read_array(0, yoffset, width, 1)
-            if row_matches.sum() == 0:
-                continue
-            row_elevation = matching_collection.elevation.read_array(0, yoffset, width, 1)
-            row_ecoregion = matching_collection.ecoregions.read_array(0, yoffset, width, 1)
-            row_slope = matching_collection.slope.read_array(0, yoffset, width, 1)
-            row_access = matching_collection.access.read_array(0, yoffset, width, 1)
-            row_luc = [
-                luc.read_array(0, yoffset, width, 1) for luc in matching_collection.lucs
-            ]
-            for xoffset in range(width):
-                if not row_matches[0][xoffset]:
+        calc = matching_collection.boundary * filtered_ecoregions * filtered_elevation * \
+            filtered_luc0 * filtered_luc5 * filtered_luc10 * filtered_slopes * filtered_access
+        count = calc.save(matching_pixels, and_sum=True)
+
+        if count > 0:
+            # now we we need to scan for matched pixels and store the data about them
+
+            width = matching_collection.boundary.window.xsize
+            row_results = []
+            for yoffset in range(matching_collection.boundary.window.ysize):
+                row_matches = matching_pixels.read_array(0, yoffset, width, 1)
+                if row_matches.sum() == 0:
                     continue
-                lucs = [x[0][xoffset] for x in row_luc]
-                results.append([
-                    xoffset,
-                    yoffset,
-                    matching_pixels.area.top + (yoffset * matching_pixels.pixel_scale.ystep),
-                    matching_pixels.area.left + (xoffset * matching_pixels.pixel_scale.xstep),
-                    row_elevation[0][xoffset],
-                    row_slope[0][xoffset],
-                    row_ecoregion[0][xoffset],
-                    row_access[0][xoffset],
-                ] + lucs)
+                row_elevation = matching_collection.elevation.read_array(0, yoffset, width, 1)
+                row_ecoregion = matching_collection.ecoregions.read_array(0, yoffset, width, 1)
+                row_slope = matching_collection.slope.read_array(0, yoffset, width, 1)
+                row_access = matching_collection.access.read_array(0, yoffset, width, 1)
+                row_luc0 = matching_collection.lucs[0].read_array(0, yoffset, width, 1)
+                for xoffset in range(width):
+                    if not row_matches[0][xoffset]:
+                        continue
+                    row_results.append(MatchedPixel(
+                        matching.lat,
+                        matching.lng,
+                        matching_pixels.area.top + (yoffset * matching_pixels.pixel_scale.ystep),
+                        matching_pixels.area.left + (xoffset * matching_pixels.pixel_scale.xstep),
+                        xoffset,
+                        yoffset,
+                        row_ecoregion[0][xoffset],
+                        row_elevation[0][xoffset],
+                        row_slope[0][xoffset],
+                        row_access[0][xoffset],
+                        row_luc0[0][xoffset],
+                    ))
+                if len(row_results) > 1000:
+                    output_queue.put(row_results)
+                    row_results = []
 
-    # now we have in matching_pixels just those that match, so we now need to store the data about them
-    return (index, count, results)
+    # Signal worker exited
+    output_queue.put(None)
 
 def find_potential_matches(
     k_filename: str,
@@ -102,45 +168,44 @@ def find_potential_matches(
     elevation_folder_filename: str,
     slope_folder_filename: str,
     access_folder_filename: str,
-    year: int,
+    start_year: int,
+    evaluation_year: int,
     result_folder: str,
 ) -> None:
     os.makedirs(result_folder, exist_ok=True)
+    result_dataframe_filename = os.path.join(result_folder, "results.parquet")
 
-    source_pixels = pd.read_parquet(k_filename)
-    k_pixels = list(source_pixels.iterrows())
-    if len(k_pixels) == 0:
-        raise ValueError("No pixels in K")
+    with Manager() as manager:
+        source_queue = manager.Queue()
+        results_queue = manager.Queue()
 
-    with Pool(processes=200) as pool:
-        results = pool.map(
-            partial(
-                find_potential_matches_per_k_item,
-                matching_zone_filename,
-                jrc_data_folder,
-                ecoregions_folder_filename,
-                elevation_folder_filename,
-                slope_folder_filename,
-                access_folder_filename,
-                year,
-            ),
-            k_pixels
-        )
-        print(f"zero results: {len([x for x in results if x[1] == 0])}")
+        consumer_process = Process(target=reduce_results, args=(
+            result_dataframe_filename,
+            results_queue,
+        ))
+        consumer_process.start()
+        workers = [Process(target=worker, args=(
+            index,
+            matching_zone_filename,
+            jrc_data_folder,
+            ecoregions_folder_filename,
+            elevation_folder_filename,
+            slope_folder_filename,
+            access_folder_filename,
+            start_year,
+            evaluation_year,
+            source_queue,
+            results_queue
+        )) for index in range(threads())]
+        for worker_process in workers:
+            worker_process.start()
+        ingest_process = Process(target=load_k, args=(k_filename, source_queue))
+        ingest_process.start()
 
-        # now we need to join all of the individual matches into one dataframe
-        columns=['x', 'y', 'lat', 'lng', 'elevation', 'slope', 'ecoregion', 'access', 'luc0', 'luc5', 'luc10']
-
-        # I suspect we're going to struggle with memory here, so just incrementally build the final array so we
-        # don't end up with two copies of everything
-        matches = pd.DataFrame(results[0][2], columns=columns)
-        for res in results[1:]:
-            next_frame = pd.DataFrame(res[2], columns=columns)
-            matches = pd.concat([matches, next_frame]).drop_duplicates(['x', 'y'])
-        print(f"Total potential match pixels: {len(matches)}")
-        result_dataframe_filename = os.path.join(result_folder, "results.parquet")
-        matches.to_parquet(result_dataframe_filename)
-
+        ingest_process.join()
+        for worker_process in workers:
+            worker_process.join()
+        consumer_process.join()
 
 def main():
     try:
@@ -151,11 +216,13 @@ def main():
         elevation_folder_filename = sys.argv[5]
         slope_folder_filename = sys.argv[6]
         access_folder_filename = sys.argv[7]
-        year = int(sys.argv[8])
-        result_folder = sys.argv[9]
+        start_year = int(sys.argv[8])
+        evaluation_year = int(sys.argv[9])
+        result_folder = sys.argv[10]
     except (IndexError, ValueError):
         print(f"Usage: {sys.argv[0]} K_PARQUET MATCHING_ZONE JRC_FOLDER "
-            "ECOREGIONS_FOLDER ELEVATION_FOLDER SLOPES_FOLDER ACCESS_FOLDER YEAR OUT",
+            "ECOREGIONS_FOLDER ELEVATION_FOLDER SLOPES_FOLDER ACCESS_FOLDER "
+            "START_YEAR EVALUATION_YEAR OUT",
             file=sys.stderr)
         sys.exit(1)
 
@@ -167,7 +234,8 @@ def main():
         elevation_folder_filename,
         slope_folder_filename,
         access_folder_filename,
-        year,
+        start_year,
+        evaluation_year,
         result_folder
     )
 
