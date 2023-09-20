eca32d968707180636ebad5a6e180940eccbaa34
Robin Message <robin@lambdacambridge.com>
2023-09-20 13:43:06 +0100

    RTree version
    Simpler to build the R-tree, but given the wide ranges we're searching for, this is inefficient

--- methods/matching/k_search_test2.py
+++ methods/matching/k_search_test2.py
@@ -284,7 +284,7 @@ def k_search_test(
     
     print("making tree...")
     start = time.time()
-    tree = make_tree(sources, np.array([elevation_width, slope_width, access_width]))
+    tree = make_rtree(sources, np.array([elevation_width, slope_width, access_width]))
     print("build time", time.time() - start)
     print("depth", tree.depth())
     print("size", tree.size())
@@ -391,9 +391,151 @@ def main():
         args.start_year,
     )
 
+class RTree:
+    def contains(self, range) -> bool:
+        raise NotImplemented()
+    
+    def depth(self) -> int:
+        return 1
+    
+    def size(self) -> int:
+        return 1
 
+    def dump(self, space: str):
+        raise NotImplemented()
+
+class RLeaf(RTree):
+    def __init__(self, point):
+        self.point = point
+    def contains(self, range) -> bool:
+        return np.all(range[0] <= self.point) & np.all(range[1] >= self.point) # type: ignore
+    def dump(self, space: str):
+        print(space, f"point {self.point}")
+
+class RFull(RTree):
+    def contains(self, range) -> bool:
+        return True
+    def dump(self, space: str):
+        print(space, f"Full")
+
+class RSplit(RTree):
+    def __init__(self, d: int, value: float, left: RTree, right: RTree):
+        self.d = d
+        self.value = value
+        self.left = left
+        self.right = right
+    def contains(self, range) -> bool:
+        l = self.value - range[0, self.d] # Amount on left side
+        r = range[1, self.d] - self.value # Amount on right side
+        # Either l or r must be positive, or both
+        # Pick the biggest first
+        if l >= r:
+            if self.left.contains(range):
+                return True
+            # Visit the rest if it is inside
+            if r >= 0:
+                if self.right.contains(range):
+                    return True
+        else:
+            if self.right.contains(range):
+                return True
+            # Visit the rest if it is inside
+            if l >= 0:
+                if self.left.contains(range):
+                    return True
+        return False
+    
+    def size(self) -> int:
+        return 1 + self.left.size() + self.right.size()
+    
+    def depth(self) -> int:
+        return 1 + max(self.left.depth(), self.right.depth())
+
+    def dump(self, space: str):
+        print(space, f"split d{self.d} at {self.value}")
+        print(space + "  <")
+        self.left.dump(space + "\t")
+        print(space + "  >")
+        self.right.dump(space + "\t")
+
+class RDrop(RTree):
+    def __init__(self, d: int, value: float, tree: RTree):
+        self.d = d
+        self.value = value
+        self.tree = tree
+    def contains(self, range) -> bool:
+        if range[0, self.d] <= self.value and range[1, self.d] >= self.value:
+            return self.tree.contains(without(range, self.d))
+        return False
+    def dump(self, space: str):
+        print(space, f"drop dimension {self.d} at {self.value} ->")
+        self.tree.dump(space + "\t")
+    def size(self):
+        return self.tree.size() + 1
+    def depth(self):
+        return self.tree.depth() + 1
+
+class RWrapper(RTree):
+    def __init__(self, tree, widths):
+        self.tree = tree
+        self.widths = widths
+    def contains(self, point) -> bool:
+        return self.tree.contains(np.array([point - self.widths, point + self.widths]))
+    def dump(self, space: str):
+        self.tree.dump(space)
+    def size(self):
+        return self.tree.size()
+    def depth(self):
+        return self.tree.depth()
+
+def make_rtree(points, expected_search_widths):
+    def make_rtree_internal(points):
+        nonlocal expected_search_widths
+        if len(points) == 1:
+            return RLeaf(points[0])
+        # Find split in dimension with most bins
+        dimensions = points.shape[1]
+        if dimensions == 0:
+            return RFull()
+        bins = None
+        chosen_d = 0
+        for d in range(dimensions):
+            d_range = np.max(points[:, d]) - np.min(points[:, d])
+            if False and d_range == 0:
+                # Great, we can drop this dimension. Doesn't currently speed things up,
+                # but we could potentially use this to handle quantized dimensions faster
+                # and to get down to a merged-list representation when only one dimension
+                # is left.
+                return RDrop(d, points[0, d], make_rtree_internal(without(points, d)))
+            d_bins = d_range / expected_search_widths[d]
+            if bins == None or d_bins > bins:
+                bins = d_bins
+                chosen_d = d
+        
+        # Split at median of d, excluding end values to avoid degenerate cases
+        split_options = np.sort(points[:, chosen_d])
+        min_split = split_options[0]
+        max_split = split_options[-1]
+        split_options = split_options[(split_options > min_split) & (split_options < max_split)]
+        if len(split_options) == 0:
+            if min_split == max_split:
+                # Logically, all of the points are the same, because the points with the widest spread
+                # have zero spread.
+                # So just match to any of the (all-equal) points.
+                # This can actually never happen because we drop duplicates earlier, but seems worth
+                # leaving in the check for robustness.
+                return RLeaf(points[0])
+            else:
+                # Only two values, so a single split will degenerate this dimension (cue Tardis noises...)
+                split_at = (max_split + min_split) / 2
+        else:
+            split_at = split_options[len(split_options) // 2]
+        lefts = points[points[:, chosen_d] <= split_at]
+        rights = points[points[:, chosen_d] > split_at]
+        return RSplit(chosen_d, split_at, make_rtree_internal(lefts), make_rtree_internal(rights))
+    return RWrapper(make_rtree_internal(np.unique(points, axis=0)), expected_search_widths)
 def test():
-    array = np.array([
+    items = np.array([
         [1, 3],
         [1, 6],
         [1, 9],
@@ -402,11 +544,14 @@ def test():
         [11, 9],
     ])
     widths = np.array([1, 2])
-    tree = make_tree(array, widths)
-    print(array)
+
+    tree = make_rtree(items, widths)
+    print(items)
     print(tree)
     tree.dump("")
-    for a in array:
+    print(tree.size())
+    print(tree.depth())
+    for a in items:
         print(tree.contains(a))
         print(tree.contains(a - (widths * 0.9)))
         print(tree.contains(a + (widths * 0.9)))
