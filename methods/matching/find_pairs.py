1c4ab5e08f214388fa0809a5e1a87aefaab1ccd9
Robin Message <robin@lambdacambridge.com>
2023-10-18 12:35:55 +0100

    Fix lint

--- methods/matching/find_pairs.py
+++ methods/matching/find_pairs.py
@@ -4,10 +4,9 @@ import random
 import logging
 from functools import partial
 from multiprocessing import Pool, cpu_count, set_start_method
-from numba import jit, float32, int32, gdb  # type: ignore
+from numba import jit  # type: ignore
 import numpy as np
 import pandas as pd
-from scipy.spatial.distance import mahalanobis  # type: ignore
 
 from methods.common.luc import luc_matching_columns
 
@@ -35,7 +34,7 @@ def find_match_iteration(
     logging.info("Find match iteration %d of %d", idx_and_seed[0] + 1, REPEAT_MATCH_FINDING)
     random.seed(idx_and_seed[1])
 
-    logging.info(f"Loading K from {k_parquet_filename}")
+    logging.info("Loading K from %s", k_parquet_filename)
 
     # Methodology 6.5.7: For a 10% sample of K
     k_set = pd.read_parquet(k_parquet_filename)
@@ -44,7 +43,7 @@ def find_match_iteration(
         random_state=random.randint(0, 1000000),
     ).reset_index()
 
-    logging.info(f"Loading S from {s_parquet_filename}")
+    logging.info("Loading S from %s", s_parquet_filename)
     s_set = pd.read_parquet(s_parquet_filename)
 
     # get the column ids for DISTANCE_COLUMNS
@@ -76,7 +75,7 @@ def find_match_iteration(
     luc_columns = [x for x in s_set.columns if x.startswith('luc')]
 
     hard_match_columns = ['country', 'ecoregion', luc10, luc5, luc0]
-    assert(len(hard_match_columns) == HARD_COLUMN_COUNT)
+    assert len(hard_match_columns) == HARD_COLUMN_COUNT
 
     # similar to the above, make the hard match columns contiguous float32 numpy arrays
     s_dist_hard = np.ascontiguousarray(s_set[hard_match_columns].to_numpy()).astype(np.int32)
@@ -85,15 +84,15 @@ def find_match_iteration(
     # Methodology 6.5.5: S should be 10 times the size of K
     required = k_set.shape[0] * 10
 
-    logging.info(f"Running make_s_subset_mask... required: {required}")
+    logging.info("Running make_s_subset_mask... required: %d", required)
 
     s_subset_mask = make_s_subset_mask(s_dist_thresholded, k_dist_thresholded, s_dist_hard, k_dist_hard, required)
 
-    logging.info(f"Done make_s_subset_mask. s_subset_mask.shape: {s_subset_mask.shape}")
+    logging.info("Done make_s_subset_mask. s_subset_mask.shape: %s", s_subset_mask.shape)
 
     s_subset = s_set[s_subset_mask].reset_index()
 
-    logging.info(f"Finished preparing s_subset. shape: {s_subset.shape}")
+    logging.info("Finished preparing s_subset. shape: %s", s_subset.shape)
 
     # Notes:
     # 1. Not all pixels may have matches
@@ -116,7 +115,8 @@ def find_match_iteration(
     # this is required so numba can vectorise the loop in greedy_match
     k_subset_match = np.ascontiguousarray(k_subset_match)
 
-    logging.info(f"Starting greedy matching... k_subset_match.shape: {k_subset_match.shape}, s_subset_match.shape: {s_subset_match.shape}")
+    logging.info("Starting greedy matching... k_subset_match.shape: %s, s_subset_match.shape: %s",
+                 k_subset_match.shape, s_subset_match.shape)
 
     add_results, k_idx_matchless = greedy_match(
         k_subset_match,
@@ -165,15 +165,21 @@ def find_match_iteration(
     logging.info("Finished find match iteration")
 
 @jit(nopython=True, fastmath=True, error_model="numpy")
-def make_s_subset_mask(s_dist_thresholded: np.ndarray, k_dist_thresholded: np.ndarray, s_dist_hard: np.ndarray, k_dist_hard: np.ndarray, n: int):
+def make_s_subset_mask(
+    s_dist_thresholded: np.ndarray,
+    k_dist_thresholded: np.ndarray,
+    s_dist_hard: np.ndarray,
+    k_dist_hard: np.ndarray,
+    number_required: int,
+):
     s_include = np.zeros((s_dist_thresholded.shape[0],), dtype=np.bool_)
     # create an array that is the indexes of the rows in s_dist_thresholded and shuffle it
     s_indexes = np.arange(s_dist_thresholded.shape[0])
     np.random.shuffle(s_indexes)
     found = 0
 
-    for p in range(s_dist_thresholded.shape[0]):
-        i = s_indexes[p]
+    for position in range(s_dist_thresholded.shape[0]):
+        i = s_indexes[position]
         s_row = s_dist_thresholded[i, :]
         s_hard = s_dist_hard[i]
 
@@ -202,7 +208,7 @@ def make_s_subset_mask(s_dist_thresholded: np.ndarray, k_dist_thresholded: np.nd
 
         if s_include[i]:
             found += 1
-            if found >= n:
+            if found >= number_required:
                 break
 
     return s_include
